f245f3e87f58ba54b274cf46932165d964664fab
melony <hjz1010@gmail.com>
2022-08-03 13:05:12 +0900

    likes App 모델 수정 및 migration 파일 추가생성

--- likes/models.py
+++ likes/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 from core.models  import TimeStampModel
 from users.models import User
-from stays.models import RoomOption
+from stays.models import Stay
 
 class Like(TimeStampModel):
-    user        = models.ForeignKey(User, on_delete=models.CASCADE)
-    room_option = models.ForeignKey(RoomOption, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stay = models.ForeignKey(Stay, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'likes'
