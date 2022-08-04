a597ba2f7c75b1c6a5e2fbca600133db7edffc7b
성바울 <bawool@seongbaul-ui-MacBookPro.local>
2022-08-04 20:04:43 +0900

    user_models 수정

--- users/models.py
+++ users/models.py
@@ -3,11 +3,12 @@ from django.db import models
 from core.models import TimeStampModel
 
 class User(TimeStampModel):
-    kakao        = models.BigIntegerField(null=True)
-    korean_name  = models.CharField(max_length=45)
-    email        = models.CharField(max_length=100, unique=True)
-    phone_number = models.CharField(max_length=45, unique=True)
+    kakao_id     = models.BigIntegerField(null=True)
+    korean_name  = models.CharField(max_length=45, null=True)
+    email        = models.CharField(max_length=100, null=True)
+    phone_number = models.CharField(max_length=45, unique=True, null=True)
     password     = models.CharField(max_length=400, null=True)
-
+    nickname     = models.CharField(max_length=45, null=True)
+    
     class Meta:
         db_table = 'users'
