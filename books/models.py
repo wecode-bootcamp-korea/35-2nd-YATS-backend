7d6f87dcf4781326a0579d59b08a7ea5ed29d23b
melony <hjz1010@gmail.com>
2022-08-06 17:36:10 +0900

    "FEAT: books App BookView POST 함수 구현"
    - 예약하기 기능 구현
    - 모델에 수정해야 할 사항이 있어서 수정
    - migration 완료

--- books/models.py
+++ books/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 from core.models  import TimeStampModel
 from users.models import User
-from stays.models import RoomOption
+from stays.models import Room
 
 class BookStatus(models.Model):
     status = models.CharField(max_length=45)
@@ -15,7 +15,7 @@ class Book(TimeStampModel):
     check_in    = models.DateField()
     check_out   = models.DateField()
     status      = models.ForeignKey(BookStatus, on_delete=models.CASCADE)
-    room_option = models.ForeignKey(RoomOption, on_delete=models.CASCADE)
+    room        = models.ForeignKey(Room, on_delete=models.CASCADE)
     user        = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
--- books/views.py
+++ books/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+import json
+import uuid
+from datetime import datetime
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from core.utils   import login_decorator   
+from books.models import Book, BookStatus
+from stays.models import Room
+
+
+class BookView(View):
+    @login_decorator
+    def post(self, request):
+        data = json.loads(request.body)
+        try: 
+            user           = request.user
+            room_id        = data['room_id']
+            check_in       = data['check_in']   
+            check_out      = data['check_out']
+            check_in_date  = datetime.strptime(check_in,'%Y-%m-%d')
+            check_out_date = datetime.strptime(check_out,'%Y-%m-%d')
+
+            room   = Room.objects.get(room_id=room_id)
+            status = BookStatus.objects.get(id=1),   
+
+            Book.objects.create(
+                book_number = uuid.uuid4().hex,
+                status      = status,
+                user        = user,
+                room        = room,  
+                check_in    = check_in_date,
+                check_out   = check_out_date
+            )
+            return JsonResponse({'message': 'SUCCESS'}, status=201)
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        except Room.DoesNotExist:
+            return JsonResponse({'message': 'INVALID_ROOM_ID'}, status=400)
