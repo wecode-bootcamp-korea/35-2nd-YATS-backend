601d944bfe5a757bba3134f12027e4bab7928c5a
melony <hjz1010@gmail.com>
2022-08-08 12:57:12 +0900

    FEAT: books App CancelView 구현
    - 예약내역에서 취소를 요청할 경우 해당 예약을 취소
    - Book table에서 해당 데이터의 status_id만 2로 변경한다

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from books.views import BookView
+from books.views import BookView, CancelView
 
 urlpatterns = [
-    path('', BookView.as_view())
+    path('', BookView.as_view()),
+    path('/cancel', CancelView.as_view())
 
 ]
