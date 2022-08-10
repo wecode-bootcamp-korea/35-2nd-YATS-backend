53cf3645ba4ca08444f40b32077401ff594ff9c0
melony <hjz1010@gmail.com>
2022-08-10 12:47:38 +0900

    FEAT: 입점문의+S3

--- stays/urls.py
+++ stays/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from stays.views import StayDetailView, RoomDetailView, FindStayView
+from stays.views import StayDetailView, RoomDetailView, FindStayView, EnterRoomView, EnterStayView
 
 urlpatterns = [
     path('findstay/<int:stay_id>', StayDetailView.as_view()),
     path('room/<str:room_name>', RoomDetailView.as_view()),
     path('findstay', FindStayView.as_view()),
+    path('entering', EnterStayView.as_view()),
+    path('entering/room', EnterRoomView.as_view())
 ]
